a3d7a76d964689089ed41a14b554aa81dfdd08be
Alexander Ha <Ha.alex.h@gmail.com>
2021-03-08 21:09:21 -0800

    initial commit

---
+++ IGDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IGDemo
+//
+//  Created by Alexander Ha on 3/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
